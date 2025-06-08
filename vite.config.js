685bd947584c7da77c6a971c3424f7cf92074827
Yuri Baseggio <yuri.baseggio@gmail.com>
2025-06-08 15:40:20 -0300

    fix: updated package.json with clean dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-  plugins: [react()]
-};
+export default defineConfig({
+  plugins: [react()],
+});
